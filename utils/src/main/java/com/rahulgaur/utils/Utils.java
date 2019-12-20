fe35082176434c95f932952f7de2d8e795a32887
Rahul Gaur <rahul.gaur152@gmail.com>
2019-12-20 21:36:12 +0530

    removed progress dialog

--- utils/src/main/java/com/rahulgaur/utils/Utils.java
+++ utils/src/main/java/com/rahulgaur/utils/Utils.java
@@ -2,18 +2,12 @@ package com.rahulgaur.utils;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.media.MediaCodec;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -374,38 +368,6 @@ public class Utils {
 
     public static void setSharedPreferenceFileName(String SHARED_PREFERENCE_FILE_NAME) {
         Utils.SHARED_PREFERENCE_FILE_NAME = SHARED_PREFERENCE_FILE_NAME;
-    }
-
-    //------------------------ Progress Bar ------------------------------------------------//
-    private static AlertDialog aDProgress;
-
-    public static void showProgressDialog(String progress, Context context) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-        View view = LayoutInflater.from(context).inflate(R.layout.pop_up_custom_progress, null);
-        builder.setCancelable(false);
-        builder.setView(view);
-
-        TextView tvProgress = view.findViewById(R.id.tvProgress);
-        tvProgress.setText(progress);
-
-        aDProgress = builder.create();
-        aDProgress.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        aDProgress.setCanceledOnTouchOutside(false);
-        aDProgress.setCancelable(false);
-        aDProgress.show();
-    }
-
-    public static void dismissDialog() {
-        if (aDProgress != null) {
-            try {
-                aDProgress.dismiss();
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.e("ProgressManager", "dismissDialog: exception with progressbar ");
-            }
-        }
     }
 
     //-----------------------------------------------------------------------------------------//
